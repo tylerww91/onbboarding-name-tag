e99a8369630bb5fce7b1dd2938a5ec21dd6d7a68
Erik M Baxstrom <baxstrem@gmail.com>
2022-09-07 15:51:29 -0700

    logs name input to console

--- app.js
+++ app.js
@@ -2,7 +2,17 @@
 
 // let state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// set event listeners
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
+
+const nameInput = document.getElementById('name-input');
+const changeNameButton = document.getElementById('change-name-button');
+const name = document.getElementById('name');
+
+changeNameButton.addEventListener('click', () => {
+    // console.log('button clicked');
+    const name = nameInput.value;
+    console.log(name);
+});
